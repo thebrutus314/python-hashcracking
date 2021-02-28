dc452a1876e40516ccfebf4fe8ba7b579f22f709
thebrutus314 <74834806+thebrutus314@users.noreply.github.com>
2021-02-28 20:04:33 +0100

    Update hashes.py

--- hashes.py
+++ hashes.py
@@ -1,4 +1,6 @@
-from urllib.request import urlopen, hashlib
+from urllib.request import urlopen, hashlib, time
+print("Codet by @thebrutus314")
+time.sleep(3)
 sha1hash = input("Please input the hash to crack.\n")
 LIST_OF_COMMON_PASSWORDS = str(urlopen("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt").read(), 'utf-8')
 for guess in LIST_OF_COMMON_PASSWORDS.split('\n'):
